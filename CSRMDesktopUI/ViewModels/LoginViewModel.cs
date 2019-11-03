72a9c8a0ddd1d872ac5a28fc1414bcb5b99548f7
christian schweighofer <bluecactusweb@gmail.com>
2019-11-03 21:56:18 +0100

    Work on LoginViewModel
    Created properties for Username and Password
    Changed maxwith for username and passwordbox

--- CSRMDesktopUI/ViewModels/LoginViewModel.cs
+++ CSRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace CSRMDesktopUI.ViewModels
 {
-    public class LoginViewModel: Screen
+    public class LoginViewModel : Screen
     {
+        private string _userName;
+        private string _password;
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value;
+                NotifyOfPropertyChange(() => UserName); }
+        }       
+        public  string Password
+        {
+            get { return _password; }
+            set { _password = value;
+                NotifyOfPropertyChange(() => Password); }                
+        }
+
     }
-}
+    }
+
