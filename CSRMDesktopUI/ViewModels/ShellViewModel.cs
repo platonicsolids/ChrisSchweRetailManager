db4ea1856cb1420b2d2353bfcf4b1c4e2be41051
christian schweighofer <bluecactusweb@gmail.com>
2019-11-03 21:39:12 +0100

    Header and LoginForm
    Created view for Login Form and the header

--- CSRMDesktopUI/ViewModels/ShellViewModel.cs
+++ CSRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace CSRMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
 
     }
 }
