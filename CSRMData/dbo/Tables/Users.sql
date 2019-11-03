b65bc0782f871f568a5f1b1bde43293f257ca713
christian schweighofer <bluecactusweb@gmail.com>
2019-11-03 17:12:07 +0100

    Register DB tables
    The tables for the basic register system

---
+++ CSRMData/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(	
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAdress] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
