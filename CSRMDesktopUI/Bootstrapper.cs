4e430b1c6dc30b5b7dfb64e1a2c9dc79613576ce
christian schweighofer <bluecactusweb@gmail.com>
2019-11-14 22:52:02 +0100

    Started with username and password validation
    Also got a helper method from stackoverflow

--- CSRMDesktopUI/Bootstrapper.cs
+++ CSRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using CSRMDesktopUI.Helpers;
 using CSRMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CSRMDesktopUI
 {
@@ -15,6 +17,10 @@ namespace CSRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
         protected override void Configure()
         {
--- CSRMDesktopUI/ViewModels/LoginViewModel.cs
+++ CSRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -9,20 +9,43 @@ namespace CSRMDesktopUI.ViewModels
 {
     public class LoginViewModel : Screen
     {
-        private string _userName;
-        private string _password;
+        private string _userName = "";
+        private string _password = "";
         public string UserName
         {
             get { return _userName; }
             set { _userName = value;
-                NotifyOfPropertyChange(() => UserName); }
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
         }       
         public  string Password
         {
             get { return _password; }
             set { _password = value;
-                NotifyOfPropertyChange(() => Password); }                
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn); 
+            }                
         }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+                if (UserName.Length > 6 && Password.Length > 6)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void LogIn(string username, string password)
+        {
+
+        }
+
 
     }
     }
