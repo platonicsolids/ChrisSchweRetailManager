637883f90c0b55ea65294827f369868410beda66
christian schweighofer <bluecactusweb@gmail.com>
2019-11-18 22:06:11 +0100

    Added Error handling during log-in

--- CSRMDesktopUI/ViewModels/LoginViewModel.cs
+++ CSRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -36,6 +36,36 @@ namespace CSRMDesktopUI.ViewModels
             }
         }
 
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+               
+            }
+        }
+
+
+
         public bool CanLogIn
         {
             get
@@ -53,14 +83,13 @@ namespace CSRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _APIHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
-
         }
     }
 }
