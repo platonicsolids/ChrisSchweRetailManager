fd33f8396bc5e6ce37d0a0abaf78aba4a3fd038e
christian schweighofer <bluecactusweb@gmail.com>
2019-12-08 22:32:42 +0100

    started with UserController -> GetById

---
+++ CSRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using CSRMDataManager.Library.DataAccess;
+using CSRMDataManager.Library.Internal.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CSRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {       
+
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId);
+           
+        }        
+    }
+}
